1e984606ddc72a356683fb53e92fe71fbf42f383
LuizTeixeira15 <luiz_ecia@hotmail.com>
2024-06-17 02:34:27 -0300

    api 1

--- api_futebol.py
+++ api_futebol.py
@@ -2,7 +2,7 @@ import requests
 import responses
 import json
 
-campeonato_id = 10
+campeonato_id = '10'
 api_futebol = "https://api.api-futebol.com.br/v1/campeonatos/"+campeonato_id+"/tabela"
 api_futebol_key = "Bearer live_fbcea0cb6695e13a022fe66a3cbced"
 
@@ -26,13 +26,13 @@ for i, c in enumerate(res):
       print(f"Pontos: {c['pontos']}")
       print(f"Jogos: {c['jogos']}")
       print(f"Vitorias: {c['vitorias']}")
-      print(f"empates: {c['empates']}")
-      print(f"derrotas: {c['derrotas']}")
-      print(f"gols_pro: {c['gols_pro']}")
-      print(f"gols_contra: {c['gols_contra']}")
-      print(f"saldo_gols: {c['saldo_gols']}")
-      print(f"aproveitamento: {c['aproveitamento']}")
-      print(f"variacao_posicao: {c['variacao_posicao']}")
-      print(f"ultimos_jogos: {c['ultimos_jogos']}")
+      print(f"Empates: {c['empates']}")
+      print(f"Derrotas: {c['derrotas']}")
+      print(f"Gols pro: {c['gols_pro']}")
+      print(f"Gols contra: {c['gols_contra']}")
+      print(f"Saldo gols: {c['saldo_gols']}")
+      print(f"Aproveitamento: {c['aproveitamento']}%")
+      print(f"Variacao posição: {c['variacao_posicao']}")
+      print(f"Ultimos jogos: {c['ultimos_jogos']}")
       
 # https://futebol.placar.esporte.uol.com.br/api/v2/debug.htm?file=commons.uol.com.br/monaco/placar/modalidades/futebol/149/2015/01/27/chelsea-x-liverpool.vm
