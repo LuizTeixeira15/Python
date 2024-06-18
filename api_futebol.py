f90bfb6f2b303f82adec80e0825100b8a5ea6661
LuizTeixeira15 <luiz_ecia@hotmail.com>
2024-06-18 07:56:21 -0300

    api 2

--- api_futebol.py
+++ api_futebol.py
@@ -35,4 +35,19 @@ for i, c in enumerate(res):
       print(f"Variacao posição: {c['variacao_posicao']}")
       print(f"Ultimos jogos: {c['ultimos_jogos']}")
       
+for i, c in enumerate(res):
+    print(f"\nPosição: {c['posicao']:02} - {c['time']['nome_popular']} - {c['pontos']} - {c['jogos']} - {c['aproveitamento']}%")
+    
+      
+      # print(f"Pontos: ")
+      # print(f"Jogos: {c['jogos']}")
+      # print(f"Vitorias: {c['vitorias']}")
+      # print(f"Empates: {c['empates']}")
+      # print(f"Derrotas: {c['derrotas']}")
+      # print(f"Gols pro: {c['gols_pro']}")
+      # print(f"Gols contra: {c['gols_contra']}")
+      # print(f"Saldo gols: {c['saldo_gols']}")
+      # print(f"Aproveitamento: {c['aproveitamento']}%")
+      # print(f"Variacao posição: {c['variacao_posicao']}")
+      # print(f"Ultimos jogos: {c['ultimos_jogos']}")
 # https://futebol.placar.esporte.uol.com.br/api/v2/debug.htm?file=commons.uol.com.br/monaco/placar/modalidades/futebol/149/2015/01/27/chelsea-x-liverpool.vm
