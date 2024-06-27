737595e0fa9cb8e5160d5a1536aaef0d58faaa2c
LuizTeixeira15 <luiz_ecia@hotmail.com>
2024-06-27 08:04:26 -0300

    api 7

--- xml.py
+++ xml.py
@@ -9,16 +9,17 @@ headers = {
   'Accept': 'application/json',
 }
 
-url = "https://script.google.com/macros/s/AKfycbySC5SkEYzn36O5CWNLMOiPTJZSN33dwDGhwYcJN_MYhSsq2ROK2JVqUq67ZLv4TRiX/exec"
+url = "https://script.google.com/macros/s/AKfycbz815KyVklSpA229FtAUWFvLD8wj6CMwslMUlqz1SPMjNiXgZZ9gx0sbMvXKMWgHBBpdQ/exec?dest=maria"
 headers={'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
 
-def buscar():
-    req = requests.get(url)
-    todos  = json.loads(req.content)
-    # print(todos)
-    for i, c in enumerate(req):
-        # print(i)
-        print(f"{i} - {todos['out'][i]['cte']}")
+# def buscar():
+req = requests.get(url)
+todos  = json.loads(req.content)
+
+print(todos)
+# for i, c in enumerate(todos['out']):
+#     if todos['out'][i]['cte'] == 22084:
+#       print(f"{i} - {todos['out'][i]['cte']}")
 
 
-buscar()
+# buscar()
