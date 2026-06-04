7667ff903b55e0f566678382ac29b88bc7aa8d9d
LuizTeixeira15 <luiz_ecia@hotmail.com>
2026-06-04 15:59:37 -0300

    XML NF

--- playwright_NF.py
+++ playwright_NF.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 
-nota = [
+notas = [
         26260512919018000170550010000385691014971816,
         27260645034218000103550010000001271310932501,
         26260515426874001154550010000263681394665003
@@ -16,27 +16,22 @@ with sync_playwright() as pw:
     contexto = navegador.new_context()
     page = contexto.new_page()
 
-    page.goto("https://meudanfe.com.br/#")    
-    page.get_by_role("textbox", name="Chave de Acesso:").fill(str(nota[1]))
-    page.get_by_role("link", name="BUSCAR", exact=True).click()
-    
-    # page.get_by_role("#downloadXmlBtn").click()
-    # page.get_by_role("#downloadDaBtn").click()
+    page.goto("https://meudanfe.com.br/#")
+    for nota in notas:
+        page.get_by_role("textbox", name="Chave de Acesso:").fill(str(nota))
+        page.get_by_role("link", name="BUSCAR", exact=True).click()
 
-    with page.expect_download() as download_info:
-        page.get_by_role("link", name="Baixar XML", exact=True).click()   
-    
-    download = download_info.value
-    download.save_as("./downloads/xml/" + download.suggested_filename)
+        with page.expect_download() as download_info:
+            page.get_by_role("link", name="Baixar XML", exact=True).click()   
+        
+        download = download_info.value
+        download.save_as("./downloads/xml/" + download.suggested_filename)
 
-    with page.expect_download() as download_info:
-        page.get_by_role("link", name="Baixar PDF", exact=True).click()   
+        with page.expect_download() as download_info:
+            page.get_by_role("link", name="Baixar PDF", exact=True).click()   
+        
+        download = download_info.value
+        download.save_as("./downloads/nf/" + download.suggested_filename)
+        page.get_by_role("link", name="NOVA CONSULTA", exact=True).click()
     
-    download = download_info.value
-    download.save_as("./downloads/nf/" + download.suggested_filename)
-    
-    
-
-
-
     navegador.close()
